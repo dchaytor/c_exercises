94fd2b412e573f7c5085e94fc5a0267bb15f41bd
diana.chaytor <dlchaytor@outlook.com>
2024-10-10 21:27:33 -0400

    refactored logic for string parsing and condition checking; added case to handle empty strings; put together a few extra test cases

--- ch13/ex2.c
+++ ch13/ex2.c
@@ -1,28 +1,29 @@
 /*  write a function that takes a single string as its argument and
     returns a pointer to the first nonwhite character in the string */
 
-
 #include <stdio.h>
 
-
-char *firstNWChar(char * stringIn, int count)   {
-    char *p = stringIn;
-
-    for (short i = 0; i < count; ++i)   {
-        if (*p != ' ') break;
-        ++p;
-    }
-
-    return p;
+char *firstNWChar(char * stringIn)   {
+	while (*stringIn != '\0')	{
+		if (*stringIn != ' ')	 break;		// break loop if non-whitespace
+    	stringIn++;
+	}
+	
+	return stringIn;	// returns either end of string char or first non-empty char
 }
 
-// i'm too lazy to do input arguments on this one
-int main(void)  {
-    char test[] = "   Asfljasl lksjdflk ";
 
-    char *p = firstNWChar(test, sizeof(test) / sizeof(char));
-    
-    printf("%c\n", *p);
+/* 	i *would* do input argument from command line, but i don't think that would work for this
+	with how whitespace is handled on cml... and ngl i hate taking arbitrary user input from
+	prompts so i'm just not going to bother!   */
+int main(void)  {
+    char test[] = "   Asfljasl lksjdflk ";	// default test case
+	//char test[] = "                    ";		// testing case of empty string
+    //char test[] = "asf";						// testing trivial case
+	
+	char *p = firstNWChar(test);
+	if (*p == '\0')	printf("String is entirely whitespace\n");
+	else printf("The first character of the string <<%s>> is <<%c>>\n", test, *p);
 
     return(0);  
 }
