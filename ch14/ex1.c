3702763b91104592e2024be6c96901a966c7bb06
diana.chaytor <dlchaytor@outlook.com>
2024-10-19 22:12:33 -0400

    refactored ex1 function logic to work with arbitrary input (i.e., should work no matter what length of line is in file) - going to benchmark different functions to make sure this way is also fastest

--- ch14/ex1.c
+++ ch14/ex1.c
@@ -1,55 +1,105 @@
 /*  write a program that reads a file and then counts the
     number of lines in it. 2023-08-15   */
 
-// should really be doing this as a bitstream --- re-read chapter and re-write
+// TRY A COUPLE DIFFERENT METHODS FOR THIS - different versions in
+	// methods and benchmark to see which works best (fastest + handles
+// weird input best. stuff to try:
+// 	- reading file in line by line using fscanf
+// 	- reading file in char by char using fgetc
+// 	- reading some of file into buffer then looping thru buffer w/
+// 	  pointer dereferences
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #define _TESTING
 #undef _TESTING
+#define BUFFER_SIZE 256
 
-int readLines(char fileName[])  {
+int readLines1(char fileName[])	{
+	// read file into a buffer string and loop through with	
+    unsigned int count = 0;
+	char buffer[BUFFER_SIZE];
+	
+	int thisFile = open(fileName, O_RDONLY);
+	//unsigned short readSize;
+	char* thisCharP;	
+	char thisChar;
+
+	while ((read(thisFile, buffer, sizeof(buffer))) > 0)	{
+		thisCharP = buffer;	 // not sure if this is right lol but we'll see
+		while (thisCharP != NULL)	{ 
+			thisChar = *thisCharP;
+			if (thisChar == '\0') break;
+			else if (thisChar == '\n') count++;
+			
+			//printf("%c", thisChar);
+			//fflush(stdout);
+			thisCharP++;
+		}
+	}
+	
+	close(thisFile);
+
+	return count;
+} // end readLines1 function
+
+int readLines2(char fileName[])	{
+    unsigned int count = 0;
+	FILE * thisFile = fopen(fileName, "r");
+	char thisChar;
+
+    if (thisFile == NULL)   {
+        fprintf(stderr, "Error opening file %s\n", fileName);
+        return -1;
+    }
+
+	while ((thisChar = fgetc(thisFile)) != EOF)	{
+		if (thisChar == '\n') count++;
+	}
+
+	fclose(thisFile);
+	return count;
+} // end readLines2 function
+
+
+int readLines3(char fileName[])  {
+    unsigned int count = 0;
     FILE * thisFile = fopen(fileName, "r");
 
     if (thisFile == NULL)   {
         fprintf(stderr, "Error opening file %s\n", fileName);
         return -1;
     }
-   
-    // it's worse for memory to do this way, but if i read in 1 char at a time and look for NL or EOF that's a lot of comparisons    
-    // if this buffer is too small (e.g., if file has lines with >120 characters) this prog throws a segfault - not sure how to 
-	// handle input safely here, maybe should just wrap the whole thing in a try-catch but that also feels hacky 
+    // if this buffer is too small (e.g., if file has lines with >120 characters)
+	// this throws a segfault - not sure how to really solve
 	char thisLine[120];
-    
-    unsigned int count = 0;
 
-    while (1)   {
-        fscanf(thisFile, "%s", thisLine);           
+    while (fscanf(thisFile, "%s", thisLine))   {
         if (getc(thisFile) == EOF) break;   // unga bunga
-        //if (thisLine[0] == EOF) break;   // unga bunga
-        
 		++count;
     }
     
     fclose(thisFile); 
     return count; 
-} // end readLines funtion
+} // end readLines3 funtion
 
 int main(int argc, char *argv[])    {
 
     int cCount = 0; // can't use unsigned since returning -ve for errors
-    //char [100] cFile; // not sure if i'll need a temp var with a typecast or not
 
     if (argc < 2)   {
         fprintf(stderr, "No arguments passed to program - aborting.\n");
         exit(8);
     } else    {   
         for (unsigned short i = 1; i < argc; ++i)   {
-            cCount = readLines(argv[i]); 
+            cCount = readLines1(argv[i]); 
             if (cCount >= 0) printf("The number of lines in %s is %d\n", argv[i], cCount);
         }
     }
     
-
     return(0);
 }
