0ad381cd2c65cc24e88b56c6b661cbdecb46fa19
diana.chaytor <dlchaytor@outlook.com>
2024-10-31 04:55:47 -0400

    roughed out code for printout, initial test case

--- ch15/ex2.c
+++ ch15/ex2.c
@@ -3,6 +3,8 @@
  * it using pointers and determine the time savings.			*/
 
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 int matMul(int rowA, int colA, int rowB, int colB, \
 		int A[rowA][colA], int B[rowB][colB], int AB[rowA][colB]);
@@ -10,13 +12,79 @@ int matMul(int rowA, int colA, int rowB, int colB, \
 int matMul_nonOpt(int rowA, int colA, int rowB, int colB, \
 	   	int A[rowA][colA], int B[rowB][colB], int AB[rowA][colB]);
 
-int main()	{
-	// FILL OUT TEST CASES
+int main(int argc, const int *argv[])	{
+	// WRITE CODE TO ACCEPT OPTIONAL INPUT FILE (default to stdout)
+	int s = 0;
+	FILE* OUTFILE = stdout;	
+	if (2 <= argc)	{
+		OUTFILE = fopen(argv[1], 'r');
+		if (NULL == OUTFILE)	{
+			fprintf("Error: could not print to file %s; aborting program",
+					argv[1]);
+			exit(-1);
+		}
+	}
 
+	// FILL OUT TEST CASES
+	time_t *AB1_START, time_t *AB1_END;
+	
+	const int ROWS_A1 = 2;
+	const int COLS_A1 = 2;
+	const int ROWS_B1 = 2;
+	const int COLS_B1 = 2;
+
+	A1 = {
+		[1, 2],
+		[5, 6]
+	};
+	B1 = {
+		[3, 3],
+		[9, 4]
+	};
+	int AB1[ROWS_A1][COLS_B1];
+	
+	// timing non-optimal function
+	prefix = "non-";
+	time(AB1_START);
+	s = matMul_nonOpt(ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, A1, B1, AB1);
+	time(AB1_end);
+	goto PRINT_MSG;		// print message out w/ current variables
+
+
+	// timing optimal function
+	prefix[0] = '\0';
+	time(AB1_start);
+	s = matMul(ROWS_A1, COLS_A1, ROWS_B2, COLS_B2, A1, B1, AB1);
+	time(AB1_end);
+	goto PRINT_MST;
 
 	return 0;
 }
+
+PRINT_MSG:
+	fprintf(OUTFILE, "Results of %soptimal matrix multiplication:\n",\
+		   	prefix);
+	goto PRINT_MATRIX; // print matrix out w/ current variables
+	fprintf(OUTFILE, "\n");
+	return;
+
+PRINT_MATRIX:
+	// size a new char matrix based on the height of the output
+	// matrix (since should be the biggest) and the width of all
+	// three matrices + room for brackets, whitespace, math
+	// notation, and a column of \n chars at the end
+
+
+	// loop through matrices separately (trusting it's sized properly)
+	// assigning the appropriate chars in output matrix (print('0'+int) 
+
 	
+	// go through matrix in order (column > row, use ptr) and print
+	// char to output file
+	
+	
+	return;
+
 /* NOTE: assuming 2D array with space already allocated			*/ 
 int matMul(int rowA, int colA, int rowB, int colB, \
 		int A[rowA][colA], int B[rowB][colB], int AB[rowA][colB])	{
