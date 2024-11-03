cd00e75dcc8de6a2f0320ce7dcba7d8d305036fb
diana.chaytor <dlchaytor@outlook.com>
2024-11-03 17:02:27 -0500

    finished debugging program; tested with more complex matrix (non-square, longer, etc.) and both functions seem to be working (tested isolated since sharing output matrix, i.e. commented out one and tried other)

--- ch15/ex2.c
+++ ch15/ex2.c
@@ -4,10 +4,9 @@
  *
  * ASSUMPTIONS:
  * - matrices are using integer values only
- * - matrix elements are <=999 (affects printout only)
+ * - matrix intput elements are <=99 (affects printout only)
  * */
 
-#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
@@ -17,12 +16,17 @@
 #define ROWS_B1 2
 #define COLS_B1 2
 
+#define ROWS_A2 3
+#define COLS_A2 6
+#define ROWS_B2 6
+#define COLS_B2 5
+
 int matMul(int rowA, int colA, int rowB, int colB, \
 		int A[rowA][colA], int B[rowB][colB], int AB[rowA][colB]);
 
 int matMul_nonOpt(int rowA, int colA, int rowB, int colB, \
 	   	int A[rowA][colA], int B[rowB][colB], int AB[rowA][colB]);
-void PRINT_MATRIX(FILE* OUTFILE, int ROWS_A, int COLS_A, \
+void PRINT_MATMUL(FILE* OUTFILE, int ROWS_A, int COLS_A, \
 		int ROWS_B, int COLS_B, int* pA, int* pB, int* pAB);
 	
 int main(int argc, const char *argv[])	{
@@ -40,6 +44,7 @@ int main(int argc, const char *argv[])	{
 
 	time_t AB_START; time_t AB_END;
 
+	// 2x2 matrix test case
 	int A1[ROWS_A1][COLS_A1] = { \
 		{1, 2},
 		{5, 6}
@@ -50,111 +55,55 @@ int main(int argc, const char *argv[])	{
 	};
 	int AB1[ROWS_A1][COLS_B1];
 
-	// timing non-optimal function
+	// more complicated matrix test case
+	int A2[ROWS_A2][COLS_A2] = { \
+		{2, 6, 3, 1, 3, 6},
+		{3, 1, 6, 4, 3, 2},
+		{3, 5, 6, 2, 3, 3}
+	};	
+	int B2[ROWS_B2][COLS_B2] = { \
+		{5, 1, 4, 3, 1},
+		{2, 3, 2, 1, 5},
+		{9, 7, 7, 4, 1},
+		{3, 7, 8, 4, 3},
+		{4, 3, 5, 1, 2},
+		{3, 4, 2, 7, 3}
+	};
+	int AB2[ROWS_A2][COLS_B2];
+
+	// timing non-optimal function on 2x2 test case
 	time(&AB_START);
-	s = matMul_nonOpt(ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, A1, B1, AB1);
+	s += matMul_nonOpt(ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, A1, B1, AB1);
 	time(&AB_END);
-	fprintf(OUTFILE, "Results of optimal matrix multiplication:\n");
-	PRINT_MATRIX(OUTFILE, ROWS_A1, COLS_A1, ROWS_B1, COLS_B1,\
-		   	A1, B1, AB1);
-	fprintf(OUTFILE, "Time elapsed: %ds\n\n", (int)(AB_END-AB_START));
+	fprintf(OUTFILE, "Results of non-optimal matrix multiplication:\n");
+	PRINT_MATMUL(OUTFILE, ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, A1, B1, AB1);
+	fprintf(OUTFILE, "Time elapsed: %fs\n\n", (float)(AB_END-AB_START));
 	
-	// timing optimal function
+	// timing optimal function on 2x2 test case
 	time(&AB_START);
-	s = matMul(ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, \
-			&A1[0][0], &B1[0][0], &AB1[0][0]);
+	s += matMul(ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, A1, B1, AB1);
 	time(&AB_END);
 	fprintf(OUTFILE, "Results of optimal matrix multiplication:\n");
-	printf("%p\n", A1);
-	printf("%d\n", A1[0][0]);
-	fflush(stdout);
-	PRINT_MATRIX(OUTFILE, ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, \
-			&A1[0][0], &B1[0][0], &AB1[0][0]);
-	fprintf(OUTFILE, "Time elapsed: %ds\n\n", (int)(AB_END-AB_START));
+	PRINT_MATMUL(OUTFILE, ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, A1, B1, AB1);
+	fprintf(OUTFILE, "Time elapsed: %fs\n\n", (float)(AB_END-AB_START));
+	
+	// timing non-optimal function on more complex test case
+	time(&AB_START);
+	s += matMul_nonOpt(ROWS_A2, COLS_A2, ROWS_B2, COLS_B2, A2, B2, AB2);
+	time(&AB_END);
+	fprintf(OUTFILE, "Results of non-optimal matrix multiplication:\n");
+	PRINT_MATMUL(OUTFILE, ROWS_A2, COLS_A2, ROWS_B2, COLS_B2, A2, B2, AB2);
+	fprintf(OUTFILE, "Time elapsed: %fs\n\n", (float)(AB_END-AB_START));
+	
+	// timing optimal function on more complex test case
+	time(&AB_START);
+	s += matMul(ROWS_A2, COLS_A2, ROWS_B2, COLS_B2, A2, B2, AB2);
+	time(&AB_END);
+	fprintf(OUTFILE, "Results of optimal matrix multiplication:\n");
+	PRINT_MATMUL(OUTFILE, ROWS_A2, COLS_A2, ROWS_B2, COLS_B2, A2, B2, AB2);
+	fprintf(OUTFILE, "Time elapsed: %fs\n\n", (float)(AB_END-AB_START));
 	
 	return s;
-}
-
-
-// THIS SUCKS, but i'm not sure of good way to do this!
-void PRINT_MATRIX(FILE* OUTFILE, int ROWS_A, int COLS_A, \
-		int ROWS_B, int COLS_B, int* pA, int* pB, int* pAB)	{
-	// getting size of printout, assigning pointers/matrix ints
-	int PRINTROWS = (ROWS_A > ROWS_B ? ROWS_A : ROWS_B);
-	int PRINTCOLS = COLS_A + 2*COLS_B + 10;
-
-	printf("%p\n", pA);
-	printf("%d\n", *pA);
-	fflush(stdout);
-	// for loop conditions would be tough to read otherwise	
-	short A_STARTCOL = 0;	// left bracket for A
-	short A_ENDCOL = A_STARTCOL + COLS_A + 1; // A right brack
-	short B_STARTCOL = A_ENDCOL + 1;	// B left brack
-	short B_ENDCOL = B_STARTCOL + COLS_B + 1; // B right brack 
-	short AB_STARTCOL = B_ENDCOL + 2;	// AB left brack
-	short AB_ENDCOL = PRINTCOLS - 2;	// AB right brack 
-	short A_STARTROW = (PRINTROWS - ROWS_A) / 2;
-	short A_ENDROW = A_STARTROW + COLS_A; 
-	short B_STARTROW = (PRINTROWS - ROWS_B) / 2;
-	short B_ENDROW = B_STARTROW + COLS_B;
-
-	for (int i = 0; i < PRINTCOLS; ++i)	{
-		for (int j = 0; j < PRINTROWS; ++j)	{
-			// i'd love to use a switch here, but need ranges
-			if (i == A_STARTCOL)	{	// A left brack
-				if (j <= A_STARTROW && j > A_ENDROW)	{
-					fprintf(OUTFILE, "[");
-				} else fprintf(OUTFILE, " ");
-			}
-			else if (i < A_ENDCOL)	{	// A matrix 
-				if (j <= A_STARTROW && j > A_ENDROW)	{
-					fprintf(OUTFILE, "%4d", *pA++);
-				} else fprintf(OUTFILE, "    ");
-			}	
-			else if (i == A_ENDCOL)	{	// A right brack
-				if (j <= A_STARTROW && j > A_ENDROW)	{
-					fprintf(OUTFILE, "]");
-				} else fprintf(OUTFILE, " ");
-			}
-			else if (i == B_STARTCOL)	{	// B left brack
-			   if (j <= B_STARTROW && j > B_ENDROW)	{
-					fprintf(OUTFILE, "[");
-			   } else fprintf(OUTFILE, " ");
-			}	   
-			else if (i < B_ENDCOL)	{	// B matrix 
-				if (j <= B_STARTROW && j > B_ENDROW)	{
-					fprintf(OUTFILE, "%4d", *pB++);
-				} else fprintf(OUTFILE, "    ");
-			}	
-			else if (i == B_ENDCOL)	{	// B right brack
-				if (j <= B_STARTROW && j > B_ENDROW)	{
-					fprintf(OUTFILE, "]");
-				} else fprintf(OUTFILE, " ");
-			}
-			else if (i == B_ENDCOL + 1)	{ // equal sign
-				if (j == PRINTROWS / 2)	{
-					fprintf(OUTFILE, "=");
-				} else fprintf(OUTFILE, " ");
-			}
-			else if (i == AB_STARTCOL)	{ // AB left brack
-				if (j <= A_STARTROW && j > A_ENDROW)	{
-					fprintf(OUTFILE, "[");
-				} else fprintf(OUTFILE, " ");
-			}
-			else if (i < AB_ENDCOL)	{	// AB matrix
-				if (j <= A_STARTROW && j > A_ENDROW)	{
-					fprintf(OUTFILE, "%4d", *pAB++);
-				} else fprintf(OUTFILE, "    ");
-			}
-			else if (i == AB_ENDCOL)	{ // AB right brack
-				if (j <= A_STARTROW && j > A_ENDROW)	{
-					fprintf(OUTFILE, "]");
-				} else fprintf(OUTFILE, " ");
-			}
-			else fprintf(OUTFILE, "\n");
-		}
-	}
-	return;
 }
 
 /* NOTE: assuming 2D array with space already allocated			*/ 
@@ -174,21 +123,23 @@ int matMul(int rowA, int colA, int rowB, int colB, \
 	// loop through and do matrix multiplication
 	int* cpA = &A[0][0];		// col pointer for A
 	int* cpB = &B[0][0];		// row pointer for B
-	int* rpB = cpB;			// column pointer for B	
+	int* rpB = cpB;				// column pointer for B	
 	int* ABp = &AB[0][0];		// index pointer for AB
-	memset(&AB[0][0], 0, sizeof(AB));
 
-	// loop through rows of A (ptr on last col from inner loop)	
-	for (; cpA <= &A[rowA][0]; cpA++)	{
+	// loop through A in sequence (go thru cols, then fall thru to next row) 
+	while (cpA <= &A[rowA-1][colA-1])	{
 		// loop through cols of B
-		for (; cpB <= &B[0][colB]; cpB++)	{
+		for (cpB = &B[0][0]; cpB <= &B[0][colB-1]; cpB++)	{
+			*ABp = 0;
 			// w/ separate ptr, go thru rows of B for current col
-			for (rpB=cpB; rpB<=cpB+colB*(rowB-1); rpB+=colB)	{
-				*ABp +=  *rpB * *cpA;
-				++cpA; 
+			for (rpB = cpB; rpB <= cpB + (colB)*(rowB-1); rpB += colB)	{
+				*ABp += *rpB * *cpA;
+				++cpA;
 			}
-			ABp++;
+			++ABp;
+			cpA -= colA;
 		}
+		cpA += colA;
 	}	
 	return 0;
 }
@@ -200,7 +151,6 @@ int matMul_nonOpt(int rowA, int colA, int rowB, int colB, \
 		fprintf(stderr, "ERROR: matrix multiplication requires the number of columns in matrix B to equal the number of rows in matrix A.\n");
 		return -1;
 	}
-
 	if (sizeof(A)*sizeof(A[0]) < sizeof(int) * rowA * colA || \
 			sizeof(B)*sizeof(B[0]) < sizeof(int) * rowB * colB)	{
 		fprintf(stderr, "ERROR: size of provided matrix does not match input row and column values\n");
@@ -220,6 +170,86 @@ int matMul_nonOpt(int rowA, int colA, int rowB, int colB, \
 			AB[aj][bi] = elsum;
 		}
 	}		
-
 	return 0;
+}
+
+// THIS SUCKS, but i'm not sure of good way to do this!
+void PRINT_MATMUL(FILE* OUTFILE, int ROWS_A, int COLS_A, \
+		int ROWS_B, int COLS_B, int* pA, int* pB, int* pAB)	{
+
+	// computing column bounds
+	short A_STARTCOL = 0;	// left bracket for A
+	short A_ENDCOL = A_STARTCOL + COLS_A + 1; // A right brack
+	short B_STARTCOL = A_ENDCOL + 1;	// B left brack
+	short B_ENDCOL = B_STARTCOL + COLS_B + 1; // B right brack 
+	short AB_STARTCOL = B_ENDCOL + 2;	// AB left brack
+	short AB_ENDCOL = AB_STARTCOL + COLS_B + 1;	// AB right brack 
+	
+	// getting overall  size of printout
+	short PRINTROWS = (ROWS_A > ROWS_B ? ROWS_A : ROWS_B);
+	short PRINTCOLS = AB_ENDCOL + 1; // don't really need but a lil clearer
+
+	// computing row bounds
+	short A_STARTROW = (PRINTROWS - ROWS_A) / 2;
+	short A_ENDROW = A_STARTROW + ROWS_A; 
+	short B_STARTROW = (PRINTROWS - ROWS_B) / 2;
+	short B_ENDROW = B_STARTROW + ROWS_B;
+
+	for (int j = 0; j < PRINTROWS; ++j)	{
+		for (int i = 0; i < PRINTCOLS; ++i)	{
+			// i'd love to use a switch here, but need ranges
+			if (i == A_STARTCOL)	{	// A left brack
+				if (j >= A_STARTROW && j < A_ENDROW)	{
+					fprintf(OUTFILE, "[");
+				} else fprintf(OUTFILE, " ");
+			}
+			else if (i < A_ENDCOL)	{	// A matrix	
+				if (j >= A_STARTROW && j < A_ENDROW)	{
+					fprintf(OUTFILE, "%3d", *pA++);
+				} else fprintf(OUTFILE, "   ");
+			}	
+			else if (i == A_ENDCOL)	{	// A right brack
+				if (j >= A_STARTROW && j < A_ENDROW)	{
+					fprintf(OUTFILE, "]");
+				} else fprintf(OUTFILE, " ");
+			}
+			else if (i == B_STARTCOL)	{	// B left brack
+			   if (j >= B_STARTROW && j < B_ENDROW)	{
+					fprintf(OUTFILE, "[");
+			   } else fprintf(OUTFILE, " ");
+			}	   
+			else if (i < B_ENDCOL)	{	// B matrix 
+				if (j >= B_STARTROW && j < B_ENDROW)	{
+					fprintf(OUTFILE, "%3d", *pB++);
+				} else fprintf(OUTFILE, "   ");
+			}	
+			else if (i == B_ENDCOL)	{	// B right brack
+				if (j >= B_STARTROW && j < B_ENDROW)	{
+					fprintf(OUTFILE, "]");
+				} else fprintf(OUTFILE, " ");
+			}
+			else if (i == B_ENDCOL + 1)	{ // equal sign
+				if (j == PRINTROWS / 2 - 1)	{
+					fprintf(OUTFILE, " = ");
+				} else fprintf(OUTFILE, "   ");
+			}
+			else if (i == AB_STARTCOL)	{ // AB left brack
+				if (j >= A_STARTROW && j < A_ENDROW)	{
+					fprintf(OUTFILE, "[");
+				} else fprintf(OUTFILE, " ");
+			}
+			else if (i < AB_ENDCOL)	{	// AB matrix
+				if (j >= A_STARTROW && j < A_ENDROW)	{
+					fprintf(OUTFILE, "%5d", *pAB++);
+				} else fprintf(OUTFILE, "     ");
+			}
+			else if (i == AB_ENDCOL)	{ // AB right brack
+				if (j >= A_STARTROW && j < A_ENDROW)	{
+					fprintf(OUTFILE, "]");
+				} else fprintf(OUTFILE, " ");
+			}
+		}
+		fprintf(OUTFILE, "\n");
+	}
+	return;
 }
