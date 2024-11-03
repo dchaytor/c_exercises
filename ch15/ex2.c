c09bf3c48d07cee5104c77407bcfa308f1f92580
diana.chaytor <dlchaytor@outlook.com>
2024-11-02 22:37:25 -0400

    fixed some bugs in ex2, still need to finish debugging

--- ch15/ex2.c
+++ ch15/ex2.c
@@ -10,81 +10,82 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
+
+#define ROWS_A1 2
+#define COLS_A1 2
+#define ROWS_B1 2
+#define COLS_B1 2
 
 int matMul(int rowA, int colA, int rowB, int colB, \
 		int A[rowA][colA], int B[rowB][colB], int AB[rowA][colB]);
 
 int matMul_nonOpt(int rowA, int colA, int rowB, int colB, \
 	   	int A[rowA][colA], int B[rowB][colB], int AB[rowA][colB]);
-
+void PRINT_MATRIX(FILE* OUTFILE, int ROWS_A, int COLS_A, \
+		int ROWS_B, int COLS_B, int* pA, int* pB, int* pAB);
+	
 int main(int argc, const char *argv[])	{
 	// WRITE CODE TO ACCEPT OPTIONAL INPUT FILE (default to stdout)
 	int s = 0;
 	FILE* OUTFILE = stdout;	
 	if (2 <= argc)	{
-		OUTFILE = fopen(argv[1], 'r');
+		OUTFILE = fopen(argv[1], "r");
 		if (NULL == OUTFILE)	{
-			fprintf("Error: could not print to file %s; aborting program",
+			fprintf(stderr, "Error: could not print to file %s; aborting program",
 					argv[1]);
 			exit(-1);
 		}
 	}
 
-	time_t *AB_START, time_t *AB_END;
+	time_t AB_START; time_t AB_END;
 
-	// defining pointers for tracking printout
-	int *pA, int *pB, int *pAB;
-	int COLS_A, int ROWS_A, int COLS_B, int ROWS_B;
-
-	const int ROWS_A1 = 2;
-	const int COLS_A1 = 2;
-	const int ROWS_B1 = 2;
-	const int COLS_B1 = 2;
-
-	A1 = {
-		[1, 2],
-		[5, 6]
+	int A1[ROWS_A1][COLS_A1] = { \
+		{1, 2},
+		{5, 6}
 	};
-	B1 = {
-		[3, 3],
-		[9, 4]
+	int B1[ROWS_B1][COLS_B1] = { \
+		{3, 3},
+		{9, 4}
 	};
-	int AB[ROWS_A1][COLS_B1];
-
-	// getting size of printout, assigning pointers/matrix ints
-	int PRINTROWS = (ROWS_A1 > ROWS_B1 ? ROWS_A1 : ROWS_B1);
-	int PRINTCOLS = COLS_A1 + 2*COLS_B1 + 10;
-	COLS_A = COLS_A1, ROWS_A = ROWS_A1;
-	COLS_B = COLS_B1, ROWS_B = ROWS_B1;
+	int AB1[ROWS_A1][COLS_B1];
 
 	// timing non-optimal function
-	prefix = "non-";
-	pA = A1, pB = B1, pAB = AB1;	// reset printout ptrs
-	time(AB_START);
+	time(&AB_START);
 	s = matMul_nonOpt(ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, A1, B1, AB1);
-	time(AB_END);
-	goto PRINT_MSG;		// print message out w/ current variables
-
+	time(&AB_END);
+	fprintf(OUTFILE, "Results of optimal matrix multiplication:\n");
+	PRINT_MATRIX(OUTFILE, ROWS_A1, COLS_A1, ROWS_B1, COLS_B1,\
+		   	A1, B1, AB1);
+	fprintf(OUTFILE, "Time elapsed: %ds\n\n", (int)(AB_END-AB_START));
+	
 	// timing optimal function
-	prefix[0] = '\0';
-	pA = A1, pB = B1, pAB = AB1;	// reset printout ptrs
-	time(AB_START);
-	s = matMul(ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, A1, B1, AB1);
-	time(AB_END);
-	goto PRINT_MSG;		// print message out w/ current variables
-
-	return 0;
+	time(&AB_START);
+	s = matMul(ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, \
+			&A1[0][0], &B1[0][0], &AB1[0][0]);
+	time(&AB_END);
+	fprintf(OUTFILE, "Results of optimal matrix multiplication:\n");
+	printf("%p\n", A1);
+	printf("%d\n", A1[0][0]);
+	fflush(stdout);
+	PRINT_MATRIX(OUTFILE, ROWS_A1, COLS_A1, ROWS_B1, COLS_B1, \
+			&A1[0][0], &B1[0][0], &AB1[0][0]);
+	fprintf(OUTFILE, "Time elapsed: %ds\n\n", (int)(AB_END-AB_START));
+	
+	return s;
 }
 
-PRINT_MSG:
-	fprintf(OUTFILE, "Results of %soptimal matrix multiplication:\n",\
-		   	prefix);
-	goto PRINT_MATRIX; // print matrix out w/ current variables
-	fprintf(OUTFILE, "Time elapsed: %ds\n\n", (int)(AB_END-AB_START));
-	return;
 
-PRINT_MATRIX:
-	// THIS SUCKS, but i'm not sure of good way to do this!
+// THIS SUCKS, but i'm not sure of good way to do this!
+void PRINT_MATRIX(FILE* OUTFILE, int ROWS_A, int COLS_A, \
+		int ROWS_B, int COLS_B, int* pA, int* pB, int* pAB)	{
+	// getting size of printout, assigning pointers/matrix ints
+	int PRINTROWS = (ROWS_A > ROWS_B ? ROWS_A : ROWS_B);
+	int PRINTCOLS = COLS_A + 2*COLS_B + 10;
+
+	printf("%p\n", pA);
+	printf("%d\n", *pA);
+	fflush(stdout);
 	// for loop conditions would be tough to read otherwise	
 	short A_STARTCOL = 0;	// left bracket for A
 	short A_ENDCOL = A_STARTCOL + COLS_A + 1; // A right brack
@@ -154,39 +155,39 @@ PRINT_MATRIX:
 		}
 	}
 	return;
+}
 
 /* NOTE: assuming 2D array with space already allocated			*/ 
 int matMul(int rowA, int colA, int rowB, int colB, \
 		int A[rowA][colA], int B[rowB][colB], int AB[rowA][colB])	{
 	// check for errors in input
-	if (col != rowB) {
-		fprintf("ERROR: matrix multiplication requires the number of rows in matrix B to equal the columns of rows in matrix A.");
+	if (colA != rowB) {
+		fprintf(stderr, "ERROR: matrix multiplication requires the number of rows in matrix B to equal the columns of rows in matrix A.\n");
 		return -1;
 	}
-
-	if (sizeof(A) < sizeof(int) * rowA * colA || \
-			sizeof(B) < sizeof(int) * rowB * colB)	{
-		fprintf("ERROR: size of provided matrix does not match input row and column values");
+	if (sizeof(A)*sizeof(A[0]) < sizeof(int) * rowA * colA || \
+			sizeof(B)*sizeof(B[0]) < sizeof(int) * rowB * colB)	{
+		fprintf(stderr, "ERROR: size of provided matrix does not match input row and column values\n");
 		return -1;		
 	}
 	
 	// loop through and do matrix multiplication
-	register int* cpA = &A[0][0];		// col pointer for A
-	register int* cpB = &B[0][0];		// row pointer for B
-	register int* rpB = cpB;			// column pointer for B	
-	register int* ABp = &AB[0][0];		// index pointer for AB
-	memset(&AB[0][0], 0, rowA * colB * sizeof(AB[0][0]));
+	int* cpA = &A[0][0];		// col pointer for A
+	int* cpB = &B[0][0];		// row pointer for B
+	int* rpB = cpB;			// column pointer for B	
+	int* ABp = &AB[0][0];		// index pointer for AB
+	memset(&AB[0][0], 0, sizeof(AB));
 
 	// loop through rows of A (ptr on last col from inner loop)	
 	for (; cpA <= &A[rowA][0]; cpA++)	{
 		// loop through cols of B
-		for (; cpB <= &B[0][colB]; cpP++)	{
+		for (; cpB <= &B[0][colB]; cpB++)	{
 			// w/ separate ptr, go thru rows of B for current col
-			for (rpB=cpB; rpB<=cpB+colB*(rowB-1); rpP+=colB)	{
-				&ABp +=  &rpB * &cpA;
+			for (rpB=cpB; rpB<=cpB+colB*(rowB-1); rpB+=colB)	{
+				*ABp +=  *rpB * *cpA;
 				++cpA; 
 			}
-			&ABp++;
+			ABp++;
 		}
 	}	
 	return 0;
@@ -196,13 +197,13 @@ int matMul_nonOpt(int rowA, int colA, int rowB, int colB, \
 	   	int A[rowA][colA], int B[rowB][colB], int AB[rowA][colB])	{
 		// check for errors in input
 	if (colA != rowB) {
-		fprintf("ERROR: matrix multiplication requires the number of columns in matrix B to equal the number of rows in matrix A.");
+		fprintf(stderr, "ERROR: matrix multiplication requires the number of columns in matrix B to equal the number of rows in matrix A.\n");
 		return -1;
 	}
 
-	if (sizeof(A) < sizeof(int) * rowA * colA || \
-			sizeof(B) < sizeof(int) * rowB * colB)	{
-		fprintf("ERROR: size of provided matrix does not match input row and column values");
+	if (sizeof(A)*sizeof(A[0]) < sizeof(int) * rowA * colA || \
+			sizeof(B)*sizeof(B[0]) < sizeof(int) * rowB * colB)	{
+		fprintf(stderr, "ERROR: size of provided matrix does not match input row and column values\n");
 		return -1;		
 	}
 	
