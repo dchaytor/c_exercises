a01b6a5874ef16ae584d430b18402a3273ed2ee7
diana.chaytor <dlchaytor@outlook.com>
2024-11-04 20:25:55 -0500

    ch15, ex3

--- ch15/ex3.c
+++ ch15/ex3.c
@@ -1,2 +1,23 @@
 /* ex15-3: write a program to sum the elements in an array.
- * optimize it. 	*/ 
+ * optimize it. 	*/
+
+#include <stdio.h>
+
+int sumArray(int A[], int sizeA)	{
+	register int sum = 0;
+	int* pA = &A[0];
+	while (pA < (A + sizeA))	{
+		sum += *pA++;
+	}
+	return sum;
+}
+
+// i could make this more usable but why bother here
+// like, could get user input from shell param or from scanf but eh
+int main(void)	{
+	int array[] = {1,2,13,234,14,13,12,3,4};
+	
+	printf("The sum of the array is %d\n", \
+			sumArray(array, sizeof(array) / sizeof(array[0])));
+	return 0;
+}
